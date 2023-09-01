c5d2fa37a5c60f426ee779a8920ef71eb782ac2d
Renan Coral <renan.coral@hotmail.com>
2023-08-31 23:27:20 -0300

    voltando para commit antigo

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicação está se conectando ao banco de dados")
-//mais um console log de teste:
-console.log("123teste")
-console.log("Enviando para o GitHub")
