637c2bb7beabd42a0987f7331be45c33a2128545
Renan Coral <renan.coral@hotmail.com>
2023-08-31 23:38:37 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("Aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("123teste")
 console.log("Enviando para o GitHub")
+console.log("Mais um commit de teste")
